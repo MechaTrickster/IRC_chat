aa0ce4eef8d88bedd8d92cc9b5f76e959aaf2053
MechaTrickster <alessandro_commodaro@hotmail.it>
2020-07-02 17:15:53 +0200

    Add files via upload

---
+++ MultiClient/MultiClient/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+
+namespace MultiClient
+{
+    class Program
+    {
+        private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, 
+                                                            SocketType.Stream, ProtocolType.Tcp);
+
+        public static RSACryptoServiceProvider rsa;
+        public static RSAParameters privateParam;
+        public static RSAParameters publicParam;
+
+        static bool once = true;
+
+        static string s = "/room";
+
+        static byte[] buffer;
+
+        public static void ThreadProc()
+        {
+            Console.Write("Inserisci il tuo nome: ");
+
+            string nickName = Console.ReadLine();
+
+            Console.WriteLine("Benvenuto " + nickName);
+
+            //legge la stringa da tastiera e la converte in byte
+
+            string dataLength;
+
+            //byte[] signature = Encoding.ASCII.GetBytes(signMessage(textLength.ToString(), privateParam));
+
+            string hash;
+            string mex;
+            
+
+            byte[] hashByte;
+            byte[] lengthByte;
+            byte[] sendPacket;
+
+            while (true)
+            {
+                if (once)
+                {
+                    Console.WriteLine("Scegli la stanza: ");
+                    mex = Console.ReadLine();
+                    buffer = Encoding.ASCII.GetBytes(s + mex);
+                    once = false;
+                }
+                else
+                {
+                    mex = Console.ReadLine();
+                    buffer = Encoding.ASCII.GetBytes(nickName + ": " + mex);
+                }
+                    
+                dataLength = buffer.Length.ToString();
+
+                hash = SignMessage(dataLength, privateParam);
+
+                hashByte = Encoding.ASCII.GetBytes(hash);
+
+                lengthByte = Encoding.ASCII.GetBytes("*/*" + dataLength + "*/*");
+
+                sendPacket = new byte[buffer.Length + lengthByte.Length + hashByte.Length];
+
+                System.Buffer.BlockCopy(buffer, 0, sendPacket, 0, buffer.Length);
+                System.Buffer.BlockCopy(lengthByte, 0, sendPacket, buffer.Length, lengthByte.Length);
+                System.Buffer.BlockCopy(hashByte, 0, sendPacket, buffer.Length + lengthByte.Length, hashByte.Length);
+
+                _clientSocket.Send(sendPacket);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            X509KeyStorageFlags flags = X509KeyStorageFlags.Exportable;
+            X509Certificate2 cert = new X509Certificate2("cert_key.p12", "finalfantasy", flags);
+
+            //esporta i parametri della chiave privata
+            //privateParam = rsa.ExportParameters(true);
+
+            var privateKey = cert.GetRSAPrivateKey();
+            //var publicKey = certp.GetRSAPublicKey();
+
+            privateParam = privateKey.ExportParameters(true);
+            //publicParam = publicKey.ExportParameters(false);
+
+            /*
+            //legge la stringa da tastiera e la converte in byte
+            byte[] data = Encoding.ASCII.GetBytes(Console.ReadLine());
+            string dataLength = data.Length.ToString();
+
+            //byte[] signature = Encoding.ASCII.GetBytes(signMessage(textLength.ToString(), privateParam));
+
+            string hash = signMessage(dataLength, privateParam);
+            byte[] hashByte = Encoding.ASCII.GetBytes(hash);
+
+
+            byte[] lengthByte = Encoding.ASCII.GetBytes("*//*"+dataLength+"*//*");
+
+            byte[] sendPacket = new byte[data.Length + lengthByte.Length + hashByte.Length];
+
+            System.Buffer.BlockCopy(data, 0, sendPacket, 0, data.Length);
+            System.Buffer.BlockCopy(lengthByte, 0, sendPacket, data.Length, lengthByte.Length);
+            System.Buffer.BlockCopy(hashByte, 0, sendPacket, data.Length + lengthByte.Length, hashByte.Length);
+            */
+
+            //string n = System.Text.Encoding.UTF8.GetString(sendPacket);
+
+            
+            //string[] splits = n.Split("*/*");
+
+            //Console.WriteLine(splits[0]);
+            //Console.WriteLine(splits[1]);
+            //Console.WriteLine(splits[2]);
+
+
+            /*
+                data, dataLenght, hash,              
+
+             */
+
+            //Console.WriteLine(Encoding.ASCII.GetString(signature));
+
+
+            /*    if (verifySignature(1+textLength.ToString(), hash, publicParam))
+                {
+                    Console.WriteLine("verificato");
+                }
+                else
+                {
+                    Console.WriteLine("NON verificato");
+                }   */
+
+
+            /*
+            String prova = signMessage("ciao", privateParam);
+
+            if(verifySignature("ciao", prova, publicParam))
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("miss");
+            }
+            */
+
+            //lettura chiave pubblica
+            //rsa = (RSACryptoServiceProvider)cert.PrivateKey;
+
+            //Console.WriteLine(privateParam);
+            //Console.WriteLine(publicParam.ToString());
+
+            //Console.WriteLine(certp);
+
+            Console.Title = "CLIENT";
+            LoopConnect();
+            Thread t = new Thread(new ThreadStart(ThreadProc));
+            t.Start();
+            SendLoop();
+            Console.ReadLine();
+        }
+
+        private static void SendLoop()
+        {
+            while (true)
+            {
+                //Thread.Sleep(500);
+                //byte[] buffer = Encoding.ASCII.GetBytes(Console.ReadLine());
+                //_clientSocket.Send(buffer);
+                byte[] receivedBuf = new byte[1024];
+                int rec = _clientSocket.Receive(receivedBuf);
+                byte[] data = new byte[rec];
+                Array.Copy(receivedBuf, data, rec);
+                Console.WriteLine("Scrive " + Encoding.ASCII.GetString(data));                
+
+            }
+        }
+
+        private static void LoopConnect()
+        {
+            int attempts = 0;
+
+            while (!_clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    _clientSocket.Connect(IPAddress.Loopback, 100);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Tentativi di connessione: " + attempts.ToString());
+                }
+            }
+
+            //converte il certificato in byte[] per l'invio
+            //byte[] certData = certp.Export(X509ContentType.Cert);
+            //_clientSocket.Send(certData);
+
+            Console.Clear();
+
+            //converte da byte[] a certificato
+            //Console.WriteLine(new X509Certificate2(certData));
+            Console.WriteLine("Connesso!");
+        }
+
+        public static String SignMessage(String value, RSAParameters privateParam)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+
+            rsa.ImportParameters(privateParam);
+
+            //crea un istanza RSAFormatter
+            RSAPKCS1SignatureFormatter RSAFormatter = new RSAPKCS1SignatureFormatter(rsa);
+
+            //setta l'algoritmo di hash col quale effettuare la firma
+            RSAFormatter.SetHashAlgorithm("SHA256");
+
+            //crea un istanza dell'algoritmo di hash
+            SHA256Managed SHhash = new SHA256Managed();
+
+            //converte la stringa in byte, ne calcola l'hash e lo firma
+            byte[] SignedHashValue = RSAFormatter.CreateSignature(SHhash.ComputeHash(new UnicodeEncoding().GetBytes(value)));
+
+            //pone il risultato ottenuto in stringa
+            string signature = System.Convert.ToBase64String(SignedHashValue);
+
+            return signature;
+        }                
+    }
+}
---
+++ MultiServer/MultiServer/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace MultiServer
+{
+    class Program
+    {
+        private static byte[] _buffer = new byte[1024];
+        private static List<Socket> _clientSockets = new List<Socket>();
+        private static Socket _serverSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public static RSAParameters publicParam;
+
+        static X509Certificate2 certp;
+
+        private static List<Utente> _utenti = new List<Utente>();
+
+
+        static void Main(string[] args)
+        {
+            certp = new X509Certificate2("public.cer");
+
+            var publicKey = certp.GetRSAPublicKey();
+
+            publicParam = publicKey.ExportParameters(false);
+
+            Console.WriteLine(certp);
+
+            Console.Title = "SERVER";
+            SetupServer();
+            Console.ReadLine();
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Avvio server...");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+            _serverSocket.Listen(1);
+
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = _serverSocket.EndAccept(AR);
+            _clientSockets.Add(socket);
+            Utente utente = new Utente();
+            utente.socket = socket;
+
+
+            _utenti.Add(utente);
+
+            Console.WriteLine("Client connesso");
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, 
+                                new AsyncCallback(ReceiveCallback), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+
+            Socket tempSocket = socket;
+
+            int received = socket.EndReceive(AR);
+            byte[] dataBuf = new byte[received];
+            Array.Copy(_buffer, dataBuf, received);
+            string text = Encoding.ASCII.GetString(dataBuf);
+
+            Console.WriteLine(text);
+
+            string[] splits = text.Split("*/*");
+
+
+            if (VerifySignature(splits[1], splits[2], publicParam))
+            {
+                Console.WriteLine("firma verificata");
+
+                if (splits[0].Contains("/room"))
+                {
+                    foreach (Utente u in _utenti)
+                    {
+                        if (u.socket == tempSocket)
+                        {
+                            string stanza = splits[0].Remove(0, 5);
+                            u.room = stanza;
+                            Console.WriteLine(u.room);
+                        }
+                    }
+                }
+                else
+                {
+                    byte[] data = Encoding.ASCII.GetBytes(splits[0]);
+
+                    string userRoom = "";
+
+                    foreach (Utente u in _utenti)
+                    {
+                        if (u.socket == tempSocket)
+                        {
+                            userRoom = u.room;
+                        }
+                    }
+
+                    foreach (Utente u in _utenti)
+                    {
+                        if (u.socket != tempSocket)
+                            if (u.room == userRoom)
+                                u.socket.BeginSend(data, 0, data.Length, SocketFlags.None,
+                    new AsyncCallback(SendCalback), u.socket);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("firma NON verificata");
+
+            }
+
+            string response = string.Empty;
+
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None,
+                                new AsyncCallback(ReceiveCallback), socket);
+        }
+
+        private static void SendCalback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+
+        public static bool VerifySignature(String value, String signature, RSAParameters publicParam)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+
+            rsa.ImportParameters(publicParam);
+
+            RSAPKCS1SignatureDeformatter RSADeformatter = new RSAPKCS1SignatureDeformatter(rsa);
+
+            RSADeformatter.SetHashAlgorithm("SHA256");
+
+            SHA256Managed SHhash = new SHA256Managed();
+
+            if (RSADeformatter.VerifySignature(SHhash.ComputeHash(new UnicodeEncoding().GetBytes(value)), System.Convert.FromBase64String(signature)))
+            {
+                //la firma è valida
+                return true;
+            }
+            else
+            {
+                //la firma non è valida
+                return false;
+            }
+        }
+    }
+}
